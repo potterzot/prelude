652b1b233270d99241c138ff1bf1f6e023e021eb
karawoo <woo.kara@gmail.com>
2014-09-06 14:40:15 -0700

    change color theme to solarized-dark

--- personal/custom.el
+++ personal/custom.el
@@ -1,9 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1989847d22966b1403bab8c674354b4a2adf6e03e0ffebe097a6bd8a32be1e19" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (prelude-require-packages '(twittering-mode
                            sublime-themes
                            ess
                            org))
 
-
+(load-theme 'solarized-dark t)
 (provide 'custom)
 
 ;;; custom.el ends here
