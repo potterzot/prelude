e9b3e2f6e3db981921790c33d6f281eda339fef0
karawoo <woo.kara@gmail.com>
2014-09-09 10:51:42 -0700

    revert changes to prelude-modules.el that broke everything

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -26,7 +26,6 @@
 ;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
-(require 'prelude-programming)
 ;; (require 'prelude-python)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-scala)
