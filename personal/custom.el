7b429b7a05a2618832791b2f0c9657e058261219
karawoo <woo.kara@gmail.com>
2014-11-05 14:16:28 -0800

    please god get rid of helm's green line highlight

--- personal/custom.el
+++ personal/custom.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(helm-selection ((t (:background "dark slate gray")))))
 
 (prelude-require-packages '(twittering-mode
                            sublime-themes
