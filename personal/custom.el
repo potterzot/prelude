92beccffb904c0b7875a341b98395a29e54362b1
Kara Woo <woo.kara@gmail.com>
2015-06-26 21:10:19 -0700

    Revert "Make text bigger (will have to see how this looks on my home laptop though)"
    This reverts commit 05e229407e81b4a38cfd7400e252d9e37acb5f32.

--- personal/custom.el
+++ personal/custom.el
@@ -12,7 +12,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
  '(font-lock-builtin-face ((t (:foreground "#B58900" :inverse-video nil :underline nil :slant normal :weight normal))))
  '(header-line ((t (:inherit mode-line :foreground "#2aa198"))))
  '(helm-candidate-number ((t (:foreground "#839496"))))
