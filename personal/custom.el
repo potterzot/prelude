3e32c759c187f1c8ba060db3a53e71bd68775fe6
Kara Woo <woo.kara@gmail.com>
2015-06-28 11:31:04 -0700

    This seems to work for exporting with XeLaTeX.
    Still need to figure out a way to switch between XeLaTeX and pdfLaTeX,
    but good enough for now.

--- personal/custom.el
+++ personal/custom.el
@@ -332,80 +332,23 @@ sentence. Otherwise kill forward but preserve any punctuation at the sentence en
 
 (require 'ox-latex)
 
-;; is it even possible to export from org mode with xelatex? signs point to no.
+;; Export from org using XeLaTeX
+;; From http://lists.gnu.org/archive/html/emacs-orgmode/2013-05/msg00975.html
 
-;;  From http://comments.gmane.org/gmane.emacs.orgmode/62340
-(setq org-e-latex-pdf-process 
-      '("latexmk -pdflatex='xelatex --shell-escape' -pdf -quiet %f"))
+;;; XeLaTeX customisations
+;; remove "inputenc" from default packages as it clashes with xelatex
+(setf org-latex-default-packages-alist
+      (remove '("AUTO" "inputenc" t) org-latex-default-packages-alist))
 
-;; From Org FAQ: http://orgmode.org/worg/org-faq.html#orgheadline185
-;; Originally taken from Bruno Tavernier: http://thread.gmane.org/gmane.emacs.orgmode/31150/focus=31432
-;; but adapted to use latexmk 4.20 or higher.
-(defun my-auto-tex-cmd ()
-  "When exporting from .org with latex, automatically run latex,
-     pdflatex, or xelatex as appropriate, using latexmk."
-  (let ((texcmd)))
-  ;; default command: oldstyle latex via dvi
-  (setq texcmd "latexmk -dvi -pdfps -quiet %f")
-  ;; pdflatex -> .pdf
-  (if (string-match "LATEX_CMD: pdflatex" (buffer-string))
-      (setq texcmd "latexmk -pdf -quiet %f"))
-  ;; xelatex -> .pdf
-  (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-      (setq texcmd "latexmk -pdflatex=xelatex -pdf -quiet %f"))
-  ;; LaTeX compilation command
-  (setq org-latex-to-pdf-process (list texcmd)))
+;; (add-to-list 'org-latex-packages-alist '("" "xltxtra" t))
+;; ;; choose Linux Libertine O as serif and Linux Biolinum O as sans-serif fonts
+;; (add-to-list 'org-latex-packages-alist '("" "libertineotf" t))
 
-(add-hook 'org-export-latex-after-initial-vars-hook 'my-auto-tex-cmd)
-
-
-;; Specify default packages to be included in every tex file, whether pdflatex or xelatex
-(setq org-latex-packages-alist
-      '(("" "graphicx" t)
-            ("" "longtable" nil)
-            ("" "float" nil)))
-
-(defun my-auto-tex-parameters ()
-      "Automatically select the tex packages to include."
-      ;; default packages for ordinary latex or pdflatex export
-      (setq org-latex-default-packages-alist
-            '(("AUTO" "inputenc" t)
-              ("T1"   "fontenc"   t)
-              (""     "fixltx2e"  nil)
-              (""     "wrapfig"   nil)
-              (""     "soul"      t)
-              (""     "textcomp"  t)
-              (""     "marvosym"  t)
-              (""     "wasysym"   t)
-              (""     "latexsym"  t)
-              (""     "amssymb"   t)
-              (""     "hyperref"  nil)))
-
-      ;; Packages to include when xelatex is used
-      (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-          (setq org-latex-default-packages-alist
-                '(("" "fontspec" t)
-                  ("" "xunicode" t)
-                  ("" "url" t)
-                  ("" "rotating" t)
-                  ("american" "babel" t)
-                  ("babel" "csquotes" t)
-                  ("" "soul" t)
-                  ("xetex" "hyperref" nil)
-                  )))
-
-      (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-          (setq org-latex-classes
-                (cons '("article"
-                        "\\documentclass[11pt,article,oneside]{memoir}"
-                        ("\\section{%s}" . "\\section*{%s}")
-                        ("\\subsection{%s}" . "\\subsection*{%s}")
-                        ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                        ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                        ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-                      org-latex-classes))))
-
-(add-hook 'org-export-latex-after-initial-vars-hook 'my-auto-tex-parameters)
+;; org to latex customisations, -shell-escape needed for minted
+(setq org-latex-pdf-process               ; for regular export
+      '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 
 ;; Collapse emphasis marks in org mode
 (setq org-hide-emphasis-markers t)
