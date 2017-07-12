0535d720a038b5148d354b06407ff32200f3bec5
Kara Woo <woo.kara@gmail.com>
2017-07-11 21:06:14 -0700

    Updating prelude changed a bunch of stuff, hopefully this is okay
    ¯\_(ツ)_/¯

--- personal/custom.el
+++ personal/custom.el
@@ -3,94 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
- '(fci-rule-color "#073642")
- '(flycheck-lintr-linters
-   " default_linters[-which(names(default_linters) %in%
-c(\"object_camel_case_linter\", \"trailing_blank_lines_linter\"))]")
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors
-   (quote
-    (("#073642" . 0)
-     ("#546E00" . 20)
-     ("#00736F" . 30)
-     ("#00629D" . 50)
-     ("#7B6000" . 60)
-     ("#8B2C02" . 70)
-     ("#93115C" . 85)
-     ("#073642" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
- '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(jabber-resource-line-format "     %r - %s")
  '(magit-diff-use-overlays nil)
  '(magit-use-overlays nil)
  '(nrepl-message-colors
    (quote
-    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(org-agenda-files
    (quote
     ("~/todo.org" "~/work/jobs/job_hunt_prep.org" "~/work/rstudio/notes.org")))
  '(package-selected-packages
    (quote
-    (ac-emoji php-mode color-theme-sanityinc-tomorrow sql-indent 2048-game zop-to-char zenburn-theme yaml-mode world-time-mode which-key wgrep volatile-highlights vkill undo-tree twittering-mode sublime-themes solarized-theme smex smartrep smartparens smart-mode-line slime scss-mode rainbow-mode rainbow-delimiters pyenv-mode polymode ox-pandoc ox-gfm ov org-ref operate-on-number mu4e-maildirs-extension move-text markdown-mode magit latex-preview-pane key-combo json-rpc json-mode js2-mode jabber imenu-anywhere ido-ubiquitous helm-projectile helm-descbinds helm-ag guru-mode grizzl google-translate god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido floobits expand-region exec-path-from-shell ess emoji-cheat-sheet-plus elpy elisp-slime-nav ein easy-kill discover-my-major diminish diff-hl csv-mode crux company-auctex company-anaconda cdlatex browse-kill-ring beacon anzu ag ack-and-a-half ace-window ace-jump-mode ace-jump-buffer)))
- '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
- '(pos-tip-background-color "#073642")
- '(pos-tip-foreground-color "#93a1a1")
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
- '(term-default-bg-color "#002b36")
- '(term-default-fg-color "#839496")
- '(vc-annotate-background nil)
- '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#c85d17")
-     (60 . "#be730b")
-     (80 . "#b58900")
-     (100 . "#a58e00")
-     (120 . "#9d9100")
-     (140 . "#959300")
-     (160 . "#8d9600")
-     (180 . "#859900")
-     (200 . "#669b32")
-     (220 . "#579d4c")
-     (240 . "#489e65")
-     (260 . "#399f7e")
-     (280 . "#2aa198")
-     (300 . "#2898af")
-     (320 . "#2793ba")
-     (340 . "#268fc6")
-     (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
- '(xterm-color-names
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
- '(xterm-color-names-bright
-   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+    (editorconfig zop-to-char zenburn-theme yaml-mode which-key wgrep volatile-highlights vkill undo-tree twittering-mode smex smartrep smartparens smart-mode-line scss-mode rainbow-mode rainbow-delimiters polymode php-mode ox-pandoc ox-gfm ov org-ref operate-on-number mu4e-maildirs-extension move-text markdown-mode magit key-combo json-mode jabber imenu-anywhere helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido floobits expand-region exec-path-from-shell ess emoji-cheat-sheet-plus elpy elisp-slime-nav ein easy-kill discover-my-major diminish diff-hl csv-mode crux company-auctex company-anaconda color-theme-sanityinc-tomorrow cdlatex browse-kill-ring beacon anzu ace-window ac-emoji)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838"))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -221,10 +150,10 @@ sentence. Otherwise kill forward but preserve any punctuation at the sentence en
 
 ;; Function to insert new code chunk in R Markdown
 ;; http://emacs.stackexchange.com/a/27419/7060
-(defun new-chunk (header) 
-  "Insert an r-chunk in markdown mode. Necessary due to interactions between polymode and yasnippet" 
-  (interactive "sHeader: ") 
-  (insert (concat "```{r " header "}\n\n```")) 
+(defun new-chunk (header)
+  "Insert an r-chunk in markdown mode. Necessary due to interactions between polymode and yasnippet"
+  (interactive "sHeader: ")
+  (insert (concat "```{r " header "}\n\n```"))
   (forward-line -1))
 
 ;; My sentences have one space after a period.
@@ -651,7 +580,7 @@ sentence. Otherwise kill forward but preserve any punctuation at the sentence en
     (insert " -*- mode: org -*-")))
 
 ;; Export quotes using \enquote{}
-(add-to-list 'org-export-smart-quotes-alist 
+(add-to-list 'org-export-smart-quotes-alist
              '("am"
                (opening-double-quote :utf-8 "“" :html "&ldquo;" :latex "\\enquote{" :texinfo "``")
                (closing-double-quote :utf-8 "”" :html "&rdquo;" :latex "}" :texinfo "''")
@@ -812,7 +741,7 @@ sentence. Otherwise kill forward but preserve any punctuation at the sentence en
 ;; In EIN mode, M-return should evaluate the current cell
 ;; Require ein-notebook first or else I get an error
 ;; https://github.com/millejoh/emacs-ipython-notebook/issues/174
-(require 'ein-notebook) 
+(require 'ein-notebook)
 (define-key ein:notebook-mode-map (kbd "M-<return>") 'ein:worksheet-execute-cell-and-goto-next)
 
 
